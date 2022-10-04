2adb5fba9a87409c1c309399b64e7736e019fd51
root <root@LAPTOP-OQGIM7NH.localdomain>
2022-10-04 15:24:37 +0200

    recieve test code esp32

--- ESP32RecieveTest/src/main.cpp
+++ ESP32RecieveTest/src/main.cpp
@@ -14,18 +14,11 @@ int status = WL_IDLE_STATUS;
 WiFiClient ethClient;
 PubSubClient client(ethClient);
 
-void callback(char *topic, byte *message, unsigned int length);
-
 void setup()
 {
   Serial.begin(9600);
   client.setServer(mqttServer, mqttPort);
   status = WiFi.begin(ssid, pass);
-  Serial.println("Connected to wifi");
-  Serial.println("\nStarting connection...");
-  client.connect(ssid, mqttUser, mqttPassword);
-  client.subscribe(mqttTopic);
-  client.setCallback(callback);
 }
 
 void callback(char *topic, byte *message, unsigned int length)
@@ -42,19 +35,28 @@ void callback(char *topic, byte *message, unsigned int length)
     }
     Serial.println();
 }
-
 void loop()
 {
   if (status = WL_CONNECTED)
   {
-    if(!client.connected())
+    client.connect(ssid, mqttUser, mqttPassword);
+    // if you get a connection, report back via serial:
+    if (client.connected())
+    {
+      // Once connected, publish an announcement
+      // ... and resubscribe
+      client.subscribe(mqttTopic);  
+      client.setCallback(callback);
+  
+    }
+    else
     {
       // if you didn't get a connection to the server:
       Serial.print("failed with state ");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-    }else{
-      client.loop();
     }
+    client.loop();
+  
   }
 }
